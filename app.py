d4e029c9ef171796a3a218a1ffe50d657f2fe2d5
Amit Rajegore <128823120+FacelessBinaryBeast@users.noreply.github.com>
2025-04-06 19:05:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import joblib
 import pandas as pd
 from train_model import predict_autism
+import os
 
 app = Flask(__name__)
 
@@ -99,7 +100,13 @@ def predict():
             'error': f'Error processing form data: {str(e)}'
         }), 400
 
-if __name__ == '__main__':
-    app.run(debug=True) 
+#if __name__ == '__main__':
+ #   app.run(debug=True) 
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
+
 
     
